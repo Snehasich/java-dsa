3da31f586bcd38b9fc91e1612a9cb1f8d2d4b0d8
Snehasich <snehasich2005@gmail.com>
2026-04-17 20:34:39 +0530

    Leetcode 219

---
+++ leetcode/easy/ContainsDuplicate2.java
@@ -0,0 +1,45 @@
+package easy;
+
+// https://leetcode.com/problems/contains-duplicate-ii/description/
+// 219. Contains Duplicate II
+
+// Example 1:
+//Input: nums = [1,2,3,1], k = 3
+//Output: true
+
+//Example 2:
+//Input: nums = [1,0,1,1], k = 1
+//Output: true
+
+//Example 3:
+//Input: nums = [1,2,3,1,2,3], k = 2
+//Output: false
+
+import java.util.*;
+public class ContainsDuplicate2 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        int k = 0;
+
+        System.out.println(containsNearbyDuplicate(nums, k));
+    }
+
+    static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+
+            set.add(nums[i]);
+
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
+
